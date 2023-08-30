25748fe747c299864c04430726730377b0e8fe65
Gonzalo Luna <lunagonzalo240mail.com>
2023-08-30 11:36:40 -0500

    subir gp

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,31 @@ import './App.css'
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [datos, setDatos] = useState()
+
+  async function getData(){
+    await fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita")
+    .then((response) => response.json())
+    .then((data) => {console.log(data), setDatos(data.drinks)})
+  }
+
+  useEffect(()=>{
+    getData();
+  },[])
 
   return (
     <>
       <h1 className="text-3xl text-cyan-300 font-bold underline">
         Hello world!
       </h1>
+      <div>
+        {datos?.map((item) => (
+          <div key={item.idDrink}>
+<h2>{item.strDrink}</h2>
+          </div>
+
+        ))}
+      </div>
     </>
   )
 }
